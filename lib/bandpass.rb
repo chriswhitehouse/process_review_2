1ebcd3fb985aadc804c293c9a9f4b84dd085e5bb
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-28 09:34:45 +0000

    Third Test Passing

--- lib/bandpass.rb
+++ lib/bandpass.rb
@@ -2,6 +2,8 @@ def bandpass(soundarray)
 
   if soundarray[0] < 40
     [40]
+  elsif soundarray[0] > 1000
+    [1000]
   else
     soundarray
   end
--- spec/bandpass_spec.rb
+++ spec/bandpass_spec.rb
@@ -8,4 +8,8 @@ describe '#bandpass' do
   it 'should recieve [30] and return [40]' do
     expect(bandpass([30])).to eq [40]
   end
+
+  it 'should recieve [1010] and return [1000]' do
+    expect(bandpass([1010])).to eq [1000]
+  end
 end
